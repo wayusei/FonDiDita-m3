479c23e5af35a803a34d7889cd36668a877f1a9c
wayusei <r.roszam@gmail.com>
2022-11-09 22:23:34 -0600

    cambios y modificaciones durante reunion

--- config/db.js
+++ config/db.js
@@ -1,26 +1,38 @@
 const { Sequelize } = require('sequelize');
 
+const Categories = require('../models/categories')
+const Customers = require('../models/customers')
+const OrderStatus = require('../models/order_status')
+const Orders_Details = require('../models/orders_details')
+const Orders = require('../models/orders')
+const Products = require('../models/products')
+const Sellers = require('../models/sellers')
+
 const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.DB_USER,
-  process.env.PASSWORD,
+  'railway',
+  'root',
+  's3GcRVgV3XzrO3clvuYW',
   {
-    host: process.env.HOST,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: { require: true, rejectUnauthorized: false }
-    },
-    native: false,
-    ssl: true,
-});
+    host: 'containers-us-west-55.railway.app',
+    dialect: 'mysql',
+    port: 7548,
+    logging: false 
+  }
+)
 
-try {
-  sequelize.authenticate();
-  console.log('La conexion fue exitosa');
-} catch (error) {
-  console.error('Hubo un problema con la conexión', error);
-}
+const models = [ 
+  Categories, 
+  Customers, 
+  OrderStatus, 
+  Orders_Details,
+  Orders,
+  Products,
+  Sellers
+  ]
 
+for(let model of models)
+  model(sequelize)
 
+//Relaciones
 
 module.exports = sequelize;
