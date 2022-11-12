35424f8455b1e9e9b812724fd4c9964f8c0f4ab0
wayusei <r.roszam@gmail.com>
2022-11-12 16:52:18 -0600

    modificaciones al swagger.js

--- config/swagger.js
+++ config/swagger.js
@@ -2,10 +2,10 @@ const options = {
     definition: {
         openapi: "3.0.0",
         info: {
-            title: "Fondidita API",
+            title: "Fondidita-m3 API",
             version: "1.0.0",
             description:
-                " Este es un CRUD API para la  Fase 2 del curso de Frontend  developmnet con Bedu. \n\n Este ejercicio simula la esctructura de una aplicación de venta de comida. Se definieron las estructura de **Customers, Products, Sellers, Orders** y las funciones que permiten agregar y borrar datos. \n\n Integrantes \n\n * [Sergio Monterrubio](https://github.com/sergiomm84) \n\n * [Angel Pimentel](https://github.com/angel-pm) \n\n * [Rodrigo Rosas](https://github.com/wayusei/) \n\n Enlaces: \n\n * [Repositorio GitHub](https://github.com/wayusei/FonDiDita) \n\n * [Servidor en heroku](https://fondidita.herokuapp.com/)",
+                "Esta es una CRUD API para la Fase 3 del curso de Desarrollo Web Fullstack Santander - Bedu. \n\n Este ejercicio simula la esctructura de una aplicación de venta de comida. Se definieron las estructuras: **Customers, Products, Sellers, Orders** y las funciones que permiten agregar y borrar datos. \n\n Integrantes \n\n * [Sergio Monterrubio](https://github.com/sergiomm84) \n\n * [Angel Pimentel](https://github.com/angel-pm) \n\n * [Rodrigo Rosas](https://github.com/wayusei/) \n\n  * [Julio Alberto Hernández](https://github.com/albertohgj/) \n\nEnlaces: \n\n * [Repositorio GitHub](https://github.com/wayusei/FonDiDita-m3) \n\n * [Servidor en Railway](https://fondidita-m3-production.up.railway.app/)",
         },
         components: {
             securitySchemes: {
